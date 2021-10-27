b1145cf6f584749f9f2cb41ac2344915d1064bde
Ben Jackson <benjacksonSE@gmail.com>
2021-10-27 11:24:44 -0400

    Makes it work

--- java.js
+++ java.js
@@ -41,100 +41,84 @@ let girlButton = document.getElementById("girlButton");
 let boyButton = document.getElementById("boyButton");
 
 let arrayOfNames = [];
-let sexChosen = "";
-let decadeChosen = "";
+let sexChosen = "M";
 
 girlButton.addEventListener("click", (e) => {
     document.body.style.backgroundColor = "pink";
     sexChosen = "F"
+    console.log(sexChosen)
 });
 
 boyButton.addEventListener("click", (e) => {
     document.body.style.backgroundColor = "rgb(92, 195, 255)";
     sexChosen = "M"
+    console.log(sexChosen)
 });
 
-babyNameForm.addEventListener("submit", (e) =>{
+submissionForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const decadeInput = e.target.decade.value;
+    const decadeInput = e.target.decade.value;;
     const startsWithInput = e.target.startsWith.value;
-    const popularityInput = e.target.currentPopularity.value;
-    const minNameLength = e.target.floorLenght.value;
+    // const popularityInput = e.target.currentPopularity.value;
+    const minNameLength = e.target.floorLength.value;
     const maxNameLength = e.target.ceilingLength.value;
     
-    const ratingInput = e.target.rating.value;
-    const commentInput = e.target["new-comment"].value;
-
-    const newRamen = {
-        name: nameInput,
-        restaurant: restaurantInput,
-        image: imageInput,
-        rating: ratingInput,
-        comment: commentInput,
-};
-
-  // const ramenMenu = document.querySelector('ramen-menu')
-    renderRamenImg(newRamen);
-    event.target.reset();
-});
-
-// fetch is interpolated to take in whatever is entered into selectedDecade
-
-fetch(`http://localhost:3000/${selectedDecade}`)
-.then((response) => response.json())
-.then((namesArr) => {
-    namesArr.forEach((namesObject) => {
-        if (sexChosen === "M") {
-            maleNamesOnly(namesObject)
-        }
-        else if(sexChosen === "F") {
-            femaleNamesOnly(namesObject);
-        }
-
-
-    });
+    fetch(`http://localhost:3000/${decadeInput}`)
+        .then((response) => response.json())
+        .then((namesArr) => {
+        // if (sexChosen === "M") {
+        //     const workingArray = namesArr.slice(100);
+        //     console.log(workingArray);
+        // } else if (sexChosen === "F") {
+        //     const workingArray = namesArr.slice(0, 99);
+        //     console.log(workingArray);
+        // }
+        namesArr.forEach((namesObject) => {
+            let currentName = namesObject.name;
+              if (
+                namesObject.sex === sexChosen &&
+                currentName.startsWith(startsWithInput) &&
+                currentName.length >= minNameLength &&
+                currentName.length <= maxNameLength
+              ) {
+                debugger;
+                arrayOfNames.push(currentName);
+                console.log(arrayOfNames);
+              }
+            
+        });
+        });
 });
 
 
-function maleNamesOnly(namesObject) {
-    if (namesObject.sex === "M") {
-        arrayOfNames.push(namesObject.name)
-        console.log(arrayOfNames)
-    }
-}
 
-function femaleNamesOnly(namesObject) {
-    if (namesObject.sex === "F") {
-        arrayOfNames.push(namesObject.name);
-        console.log(arrayOfNames)
-    }
-}
 
-function nameByFirstLetter(letterInput, namesObject) {
-    const currentName = namesObject.name
-    if (currentName.startsWith(letterInput)) {
-        startsWithArray.push(currentName)
-        console.log(startsWithArray);
-    }
-}
 
-function nameByLongerThen(lengthInput, namesObject) {
-    const currentName = namesObject.name
-    if (currentName.length >= lengthInput) {
-        longerThanArray.push(currentName)
+// function nameByFirstLetter(letterInput, namesObject) {
+//     const currentName = namesObject.name
+//     if (currentName.startsWith(letterInput)) {
+//         startsWithArray.push(currentName)
+//         console.log(startsWithArray);
+//     }
+// }
+
+// function nameByLongerThen(lengthInput, namesObject) {
+//     const currentName = namesObject.name
+//     if (currentName.length >= lengthInput) {
+//         longerThanArray.push(currentName)
         
         
-        console.log(longerThanArray)
-    }
-}
+//         console.log(longerThanArray)
+//     }
+// }
 
-function nameByShorterThen(lengthInput, namesObject) {
-    const currentName = namesObject.name
-    if (currentName.length <= length) {
-        shorterThanArray.push(currentName)
-        console.log(shorterThanArray)
-    }
-}
+// function nameByShorterThen(lengthInput, namesObject) {
+//     const currentName = namesObject.name
+//     if (currentName.length <= length) {
+//         shorterThanArray.push(currentName)
+//         console.log(shorterThanArray)
+//     }
+// }
 
 
